5ae2069db9a045fa24f145bd342a51a3442bc3e7
Peter Farber <farberpete@gmail.com>
2023-09-10 18:17:26 -0400

    Host

--- setup-dev.sh
+++ setup-dev.sh
@@ -9,3 +9,8 @@ kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/st
 kubectl create -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml
 kubectl apply -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
 
+
+helm repo add ingress-nginx https://kubernetes.github.io/ingress-nginx
+helm repo update ingress-nginx
+helm repo add jetstack https://charts.jetstack.io
+helm repo update jetstack
