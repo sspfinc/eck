a4e235cb8ea53fd7885b7157f2f5112aaf900222
Peter Farber <farberpete@gmail.com>
2023-09-10 15:16:52 -0400

    Argo Elastic Kibana

---
+++ configure-argo.sh
@@ -0,0 +1,19 @@
+while getopts p: flag
+do
+    case "${flag}" in
+        p) password=${OPTARG};;
+    esac
+done
+
+ADMIN_USER="admin"
+ADMIN_PASSWD="$(kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath="{.data.password}" | base64 -d)"
+
+argocd login localhost:8080 --username $ADMIN_USER --password $ADMIN_PASSWD --insecure
+
+argocd repo add https://github.com/sspfinc/eck --username sspfinc --password $password
+
+argocd app create applications \
+--repo https://github.com/sspfinc/kubernetes.git \
+--path apps \
+--dest-server https://kubernetes.default.svc \
+--dest-namespace default
---
+++ setup-prod.sh
@@ -0,0 +1,11 @@
+
+# ArgoCd
+
+kubectl create namespace argocd
+kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/ha/install.yaml
+
+# ECK 
+
+kubectl create -f https://download.elastic.co/downloads/eck/2.9.0/crds.yaml
+kubectl apply -f https://download.elastic.co/downloads/eck/2.9.0/operator.yaml
+
